078598067929a065782fa3cc617aa14cb7717d9b
kanghee <kanghee.cho7@gmail.com>
2021-01-08 20:42:06 -0800

    Initial Commit

---
+++ courseraCWeek4/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  courseraCWeek4
+//
+//  Created by kanghee on 1/8/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
